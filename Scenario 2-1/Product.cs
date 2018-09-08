7e0ef820fa2bc752d3f9e9f0034ecfd6b7ab91ff
William Ku <kucheech@gmail.com>
2018-09-08 21:28:14 +0800

    Add project files.

---
+++ Scenario 2-1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Scenario_2_1
+{
+    class Product
+    {
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new ArgumentNullException("Name cannot be empty or null");
+                }
+
+                name = value;
+            }
+        }
+    }
+}
---
+++ Scenario 2-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Scenario_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            p.Name = "";
+            Console.WriteLine(p.Name);
+        }
+    }
+}
